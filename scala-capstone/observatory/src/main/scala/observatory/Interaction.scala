a979b630ec95143161febfdd80595de9446d74c7
Santiago Coffey <scoffey@itba.edu.ar>
2021-07-19 09:44:06 -0400

    [scala-capstone] Add visualization programming assignment (milestone 3)

--- scala-capstone/observatory/src/main/scala/observatory/Interaction.scala
+++ scala-capstone/observatory/src/main/scala/observatory/Interaction.scala
@@ -1,6 +1,9 @@
 package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
+import observatory.Visualization.{interpolateColor, predictTemperature}
+
+import scala.math._
 
 /**
   * 3rd milestone: interactive visualization
@@ -12,7 +15,9 @@ object Interaction extends InteractionInterface {
     * @return The latitude and longitude of the top-left corner of the tile, as per http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
     */
   def tileLocation(tile: Tile): Location = {
-    ???
+    val n = pow(2, tile.zoom)
+    val r = atan(sinh(Pi * (1 - 2 * tile.y / n)))
+    Location(r * 180 / Pi, tile.x / n * 360 - 180)
   }
 
   /**
@@ -22,7 +27,25 @@ object Interaction extends InteractionInterface {
     * @return A 256×256 image showing the contents of the given tile
     */
   def tile(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)], tile: Tile): Image = {
-    ???
+    val width = 256
+    val height = 256
+
+    val baseZoom = 8
+    val (x0, y0) = (tile.x * (1 << baseZoom), tile.y * (1 << baseZoom))
+
+    val coords = for {
+      y <- 0 until height
+      x <- 0 until width
+    } yield (x, y)
+
+    val pixels = coords
+      .map({ case (x, y) => tileLocation(Tile(x + x0, y + y0, tile.zoom + baseZoom)) })
+      .map(predictTemperature(temperatures, _))
+      .map(interpolateColor(colors, _))
+      .map(color => Pixel(color.red, color.green, color.blue, 255))
+      .toArray
+
+    Image(width, height, pixels)
   }
 
   /**
@@ -36,7 +59,12 @@ object Interaction extends InteractionInterface {
     yearlyData: Iterable[(Year, Data)],
     generateImage: (Year, Tile, Data) => Unit
   ): Unit = {
-    ???
+    for {
+      (year, data) <- yearlyData
+      zoom <- 0 to 3
+      x <- 0 until (1 << zoom)
+      y <- 0 until (1 << zoom)
+    } generateImage(year, Tile(x, y, zoom), data)
   }
 
 }
