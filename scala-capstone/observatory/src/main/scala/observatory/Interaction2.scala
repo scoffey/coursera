0334d10ad039c41249025159dcbe97765dba8456
Santiago Coffey <scoffey@itba.edu.ar>
2021-07-25 13:33:23 -0400

    [scala-capstone] Add interactive user interface programming assignment (milestone 6)

--- scala-capstone/observatory/src/main/scala/observatory/Interaction2.scala
+++ scala-capstone/observatory/src/main/scala/observatory/Interaction2.scala
@@ -5,20 +5,39 @@ package observatory
   */
 object Interaction2 extends Interaction2Interface {
 
+  val TemperatureColorScale = List(
+    (60.0, Color(255, 255, 255)),
+    (32.0, Color(255, 0, 0)),
+    (12.0, Color(255, 255, 0)),
+    (0.0, Color(0, 255, 255)),
+    (-15.0, Color(0, 0, 255)),
+    (-27.0, Color(255, 0, 255)),
+    (-50.0, Color(33, 0, 107)),
+    (-60.0, Color(0, 0, 0))
+  ) // from milestone 2 color scale table
+
+  val DeviationColorScale = List(
+    (7.0, Color(0, 0, 0)),
+    (4.0, Color(255, 0, 0)),
+    (2.0, Color(255, 255, 0)),
+    (0.0, Color(255, 255, 255)),
+    (-2.0, Color(0, 255, 255)),
+    (-7.0, Color(0, 0, 255))
+  ) // from milestone 5 color scale table
+
   /**
     * @return The available layers of the application
     */
-  def availableLayers: Seq[Layer] = {
-    ???
-  }
+  def availableLayers: Seq[Layer] = List(
+    Layer(LayerName.Temperatures, TemperatureColorScale, 1975 to 2015),
+    Layer(LayerName.Deviations, DeviationColorScale, 1990 to 2015)
+  )
 
   /**
     * @param selectedLayer A signal carrying the layer selected by the user
     * @return A signal containing the year bounds corresponding to the selected layer
     */
-  def yearBounds(selectedLayer: Signal[Layer]): Signal[Range] = {
-    ???
-  }
+  def yearBounds(selectedLayer: Signal[Layer]): Signal[Range] = Signal(selectedLayer.apply().bounds)
 
   /**
     * @param selectedLayer The selected layer
@@ -28,27 +47,32 @@ object Interaction2 extends Interaction2Interface {
     *         this method should return the closest value that is included
     *         in the `selectedLayer` bounds.
     */
-  def yearSelection(selectedLayer: Signal[Layer], sliderValue: Signal[Year]): Signal[Year] = {
-    ???
-  }
+  def yearSelection(selectedLayer: Signal[Layer], sliderValue: Signal[Year]): Signal[Year] = Signal({
+    val bounds = yearBounds(selectedLayer).apply()
+    sliderValue.apply().max(bounds.start).min(bounds.end)
+  })
 
   /**
     * @param selectedLayer The selected layer
     * @param selectedYear The selected year
     * @return The URL pattern to retrieve tiles
     */
-  def layerUrlPattern(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = {
-    ???
-  }
+  def layerUrlPattern(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = Signal({
+    val layerId = selectedLayer.apply().layerName.id
+    val year = yearSelection(selectedLayer, selectedYear).apply()
+    s"target/$layerId/$year/{z}/{x}-{y}.png"
+  })
 
   /**
     * @param selectedLayer The selected layer
     * @param selectedYear The selected year
     * @return The caption to show
     */
-  def caption(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = {
-    ???
-  }
+  def caption(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = Signal({
+    val layerId = selectedLayer.apply().layerName.id.capitalize
+    val year = yearSelection(selectedLayer, selectedYear).apply()
+    s"$layerId ($year)"
+  })
 
 }
 
